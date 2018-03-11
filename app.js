847b82d35d476b9d2f424b60baeaa815a821e665
Tyler Holloway <tylerholloway@Tylers-MacBook-Pro.local>
2018-03-11 14:07:15 -0400

    Added stylesheets and removed space below navbar

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var express = require("express");
 var app = express();
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+
 
 app.get("/", function(req, res){
 	res.render("login");
